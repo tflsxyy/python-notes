1a2febb5ae2d5e11b90d544780648259406d940f
tflsxyy <16307130030@fudan.edu.cn>
2022-01-31 01:10:17 -0500

    Upload python example file

---
+++ example.py
@@ -0,0 +1,10 @@
+# 这是一行注释，写什么都可以，程序不会运行注释里的内容
+
+import os
+
+array = ["聪明", "成熟", "可爱", "漂亮", "努力"]
+print("cyj, 我喜欢你")
+
+for i in range(len(array)):
+    a = input()
+    print("%s" % (array[i]), end = "")
